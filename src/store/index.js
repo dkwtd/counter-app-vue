a7c770c460fe9d78441ce4a0213996b0989afcf4
dkwtd <onlineproject18@gmail.com>
2023-07-12 00:49:21 +0700

    almost done, error increase button yang belum

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,41 @@
 import { createStore } from 'vuex'
+import axios from 'axios'
 
 export default createStore({
   state: {
+    counter: 0,
+    colorCode: 'blue'
   },
   getters: {
+    counterSquared(state) {
+      return state.counter * state.counter
+    }
   },
   mutations: {
+    decreaseCounter(state, randomNumber) {
+      state.counter -= randomNumber
+    },
+    increaseCounter(state, randomNumber) {
+      state.counter += randomNumber
+    },
+    setColorCode(state, newValue) {
+      state.colorCode = newValue
+    }
   },
   actions: {
+    decreaseCounter({ commit }) {
+      axios('https://www.random.org/integers/?num=1&min=1&max=6&col=1&base=10&format=plain&rnd=new').then(response => {
+       commit('decreaseCounter', response.data)
+      })
+    },
+    setColorCode({commit}, newValue) {
+      commit('setColorCode', newValue) 
+    }
+  },
+    increaseCounter({ commit }) {
+      axios('https://www.random.org/integers/?num=1&min=1&max=6&col=1&base=10&format=plain&rnd=new').then(response => {
+      commit('increaseCounter', response.data)
+      })
   },
   modules: {
   }
